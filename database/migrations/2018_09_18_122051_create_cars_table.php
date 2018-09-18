b9fe3808547500ac771f29199f07b62696225b5f
Amr Mostafa Elshehaly <amshehaly@gmail.com>
2018-09-18 16:16:48 +0200

    pushing tables

--- database/migrations/2018_09_18_122051_create_cars_table.php
+++ database/migrations/2018_09_18_122051_create_cars_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCarsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('cars', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('driver_name');
+            $table->string('plate_no')->unique();
             $table->integer('age');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('model');
             $table->string('agency');
-            // $table->rememberToken();
-            // $table->timestamps();
+            $table->timestamps();
         });
     }
 
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('cars');
     }
 }
---
+++ database/migrations/2018_09_18_123750_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->Integer('car_id');
+            $table->timestamps();
+
+            // $table->primary(['user_id', 'car_id']);
+
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade');
+            $table->foreign('car_id')->references('id')->on('cars')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
